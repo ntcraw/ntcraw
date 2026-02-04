bbbea7849bdd9c443b5c19bc5249156282b5f902
ntcraw <5528985@qq.com>
2026-02-04 21:17:12 +0800

    Delete lib directory

--- lib/feature-flags.ts
+++
@@ -1,6 +0,0 @@
-export const isFeatureEnabled = (flag: string): boolean => {
-  const value = process.env[flag];
-  return value === 'true' || value === '1';
-};
-
-export const MVP_LLM_INFRA_LEARNING = isFeatureEnabled('MVP_LLM_INFRA_LEARNING');
